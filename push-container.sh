b17ff974495f3e9a9dc1fe104bb25f11f5aa311e
Brian Yip <brian.yip@marinerinnovations.com>
2023-01-28 10:33:53 -0400

    Github actions test.

--- push-container.sh
+++ push-container.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+CONTAINER_NAME=$1
+
 xhunt-build-push-container() {
-    CONTAINER_NAME=xhunt
+    # CONTAINER_NAME=xhunt
     CONTAINER_TAG=latest
+
+    # TODO Fix CI/CD and rename this to xhunt-tools
     ECR_REPO_NAME=xhunt-ecr-repo
     aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin \
         $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
