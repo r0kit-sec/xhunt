363c48140a1afd2ff5b10a090eba65e467211e22
Brian Yip <brian.yip@marinerinnovations.com>
2023-01-29 10:22:02 -0400

    Staging tmp changes

--- cloudformation/sam/job-dispatcher/job_dispatcher_function/job_dispatcher/app.py
+++ cloudformation/sam/job-dispatcher/job_dispatcher_function/job_dispatcher/app.py
@@ -1,39 +1,117 @@
-# import requests
+import boto3
+import logging
+import os
+from botocore.exceptions import ClientError
 
-from model.aws.ec2 import Marshaller
-from model.aws.ec2 import AWSEvent
-from model.aws.ec2 import EC2InstanceStateChangeNotification
+
+def process_sqs_message(message):
+    for record in event['Records']:
+        print("test")
+        payload = record["body"]
+        print(str(payload))
+
+
+def get_queue(sqs, name):
+    """
+    Gets an SQS queue by name.
+
+    :param name: The name that was used to create the queue.
+    :return: A Queue object.
+    """
+    try:
+        queue = sqs.get_queue_by_name(QueueName=name)
+        logging.info("Got queue '%s' with URL=%s", name, queue.url)
+    except ClientError as error:
+        logging.exception("Couldn't get queue named %s.", name)
+        raise error
+    else:
+        return queue
+
+
+def receive_messages(queue, max_number, wait_time):
+    """
+    Receive a batch of messages in a single request from an SQS queue.
+
+    :param queue: The queue from which to receive messages.
+    :param max_number: The maximum number of messages to receive. The actual number
+                       of messages received might be less.
+    :param wait_time: The maximum time to wait (in seconds) before returning. When
+                      this number is greater than zero, long polling is used. This
+                      can result in reduced costs and fewer false empty responses.
+    :return: The list of Message objects received. These each contain the body
+             of the message and metadata and custom attributes.
+    """
+    try:
+        messages = queue.receive_messages(
+            MessageAttributeNames=['All'],
+            MaxNumberOfMessages=max_number,
+            WaitTimeSeconds=wait_time
+        )
+        for msg in messages:
+            logging.info("Received message: %s: %s", msg.message_id, msg.body)
+    except ClientError as error:
+        logging.exception("Couldn't receive messages from queue: %s", queue)
+        raise error
+    else:
+        return messages
+
+
+def map_to_receipt_entries(messages):
+    return [{'Id': m.message_id, 'ReceiptHandle': m.receipt_handle} for m in messages]
+
+
+def cleanup_messages(queue, messages):
+    receipt_entries = map_to_receipt_entries(messages)
+    logging.info("Deleting messages...")
+    response = queue.delete_messages(Entries=receipt_entries)
+    logging.info(response)
+
+
+def fetch_queue_parameter():
+    queue_name = os.environ["REFLECTED_XSS_QUEUE_SSM_PARAMETER"]
+    ssm = boto3.client('ssm')
+    response = ssm.get_parameter(Name=queue_name, WithDecryption=False)
+    result = response['Parameter']['Value'].split('/')[-1]
+    logging.info(response)
+    return result
+
+
+def delete_message(queue, message):
+    try:
+        logging.info("Deleting message...")
+        response = queue.delete_messages(Entries=[{'Id': message.message_id, 'ReceiptHandle': message.receipt_handle}])
+        logging.info(response)
+    except ClientError as error:
+        logging.exception(f"Failed to delete message with id {message.message_id}: {error}")
+
+
+def submit_jobs(client, queue, messages):
+    # Jobs are submitted in batches to optimize and distribute workload in the cluster
+    for message in messages:
+        try:
+            response = client.submit_job(
+                jobName=f'dalfox-{message.message_id}',
+                jobQueue='test-job-queue',
+                jobDefinition='test-job-definition',
+                parameters={},
+            )
+            logging.info(f"Submitted job: {response['jobName']}")
+            delete_message(queue, message)
+        except ClientError as error:
+            logging.exception(f"Failed to submit job for message id {message.message_id}: {error}")
 
 
 def lambda_handler(event, context):
-    """Sample Lambda function reacting to EventBridge events
+    logger = logging.getLogger()
+    if 'DEBUG' in os.environ and os.environ['DEBUG']:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    logger.info('## ENVIRONMENT VARIABLES')
+    logger.info(os.environ)
+    logger.info('## EVENT')
+    logger.info(event)
 
-    Parameters
-    ----------
-    event: dict, required
-        Event Bridge EC2 State Change Events Format
-
-        Event doc: https://docs.aws.amazon.com/eventbridge/latest/userguide/event-types.html#ec2-event-type
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-        The same input event file
-    """
-
-    #Deserialize event into strongly typed object
-    awsEvent:AWSEvent = Marshaller.unmarshall(event, AWSEvent)
-    ec2StateChangeNotification:EC2InstanceStateChangeNotification = awsEvent.detail
-
-    #Execute business logic
-    print("Instance " + ec2StateChangeNotification.instance_id + " transitioned to " + ec2StateChangeNotification.state)
-
-    #Make updates to event payload
-    awsEvent.detail_type = "HelloWorldFunction updated event of " + awsEvent.detail_type;
-
-    #Return event for further processing
-    return Marshaller.marshall(awsEvent)
+    messages = event['Records']
+    logger.info(f"Got messages: {messages}")
+    # submit_jobs(batch, queue, messages)
