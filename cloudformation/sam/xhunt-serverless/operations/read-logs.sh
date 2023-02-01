07f525686c93f63ab2ed4f6a3c67ba54cf685cd7
r0kit <r0kit@r0kithax.com>
2023-01-31 22:26:22 -0400

    Read logs processed within a day from now.

--- cloudformation/sam/xhunt-serverless/operations/read-logs.sh
+++ cloudformation/sam/xhunt-serverless/operations/read-logs.sh
@@ -2,7 +2,8 @@
 
 
 LOG_GROUP=/xhunt/task-cluster/dalfox-reflected-xss
-REGION=ca-central-1
+NOW=`date +%s`000
+YESTERDAY=$((${NOW} - 86400000))
 
-aws logs filter-log-events --log-group-name $LOG_GROUP --region $REGION | jq '.events[].message'
+aws logs filter-log-events --log-group-name $LOG_GROUP --region $AWS_REGION --start-time $YESTERDAY --end-time $NOW | jq '.events[].message'
 # aws logs filter-log-events --log-group-name my-group [--log-stream-names LIST_OF_STREAMS_TO_SEARCH] [--start-time 1482197400000] [--end-time 1482217558365] [--filter-pattern VALID_METRIC_FILTER_PATTERN]
